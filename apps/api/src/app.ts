ab4a468fa0c3f68872d56cf8190fa583d2f3b296
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-03-23 19:23:26 +0500

    Feat(api): add error handling

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,1 +1,7 @@
 import "./start";
+import { errorHandler } from "./errorHandler";
+
+process.on("unhandledRejection", errorHandler);
+process.on("uncaughtException", errorHandler);
+process.on("uncaughtExceptionMonitor", errorHandler);
+
