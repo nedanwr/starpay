109a6718aab540baf4e1ae63a61be8bf1bbf1e37
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-03-23 19:21:19 +0500

    Feat(api): send welcome message

--- apps/api/src/start.ts
+++ apps/api/src/start.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { __prod__, env, port } from "./lib/constants";
 
 const app: Application = express();
@@ -11,4 +11,10 @@ if (!__prod__) {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(port, () => console.log(`Server started in ${env} mode on port ${port}`));
+app.get("/", (_req: Request, res: Response) =>
+    res.status(200).json({ message: "Welcome to StarPay API" })
+);
+
+app.listen(port, () =>
+    console.log(`Server started in ${env} mode on port ${port}`)
+);
