9b395de355f8a7a2e42ec37b8a23abcf045c56af
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-03-25 22:17:19 +0500

    Prettier(api): format

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -4,4 +4,3 @@ import { errorHandler } from "./errorHandler";
 process.on("unhandledRejection", errorHandler);
 process.on("uncaughtException", errorHandler);
 process.on("uncaughtExceptionMonitor", errorHandler);
-
